c4673fac55f36414d2b0b3653c5ebc7d12f39d1a
jazmin <correo@example>
2022-12-14 08:42:40 -0500

    button normal

---
+++ lib/ui/widgets/button_normal_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ButtonNormalWidget extends StatelessWidget {
+  Function onPressed;
+
+  ButtonNormalWidget({required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 25.0,
+      width: double.infinity,
+      child: ElevatedButton.icon(
+        onPressed: () {
+          onPressed();
+        },
+        icon: Icon(Icons.save),
+        label: 
+        style: ElevatedButton.styleFrom(
+            primary: kBrandPrymaryColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(14.0),
+            )),
+      ),
+    );
+  }
+}
